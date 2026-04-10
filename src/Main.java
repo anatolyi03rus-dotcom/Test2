06424ac6f2d9de1a2e5dc39241d3a4c84d82820a
Anatoly <anatolyi03rus@gmail.com>
2026-04-11 04:23:14 +0800

    b

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,61 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-import java.util.Scanner;
+//import java.util.Scanner;
 //Ввести пароль из командной строки и сравнить его со строкой-образцом.
+//public class Main {
+ //   public static void main(String[] args) {
+       // String samplePassword = "пароль999";
+
+    //    Scanner scanner = new Scanner(System.in);
+
+     //   System.out.print("Введите пароль: ");
+     //   String inputPassword = scanner.nextLine();
+//
+    //    if (inputPassword.equals(samplePassword)) {
+   //         System.out.println("Пароль верный");
+   //     } else {
+   //         System.out.println("Пароль неверный");
+   //     }
+ //   }
+//
+
+
 public class Main {
+    //Найти корни квадратного уравнения. Параметры уравнения передавать с командной строкой
     public static void main(String[] args) {
-        String samplePassword = "пароль999";
 
-        Scanner scanner = new Scanner(System.in);
+        if (args.length != 3) {
+            System.out.println("Использование: java Main a b c");
+            return;
+        }
 
-        System.out.print("Введите пароль: ");
-        String inputPassword = scanner.nextLine();
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
 
-        if (inputPassword.equals(samplePassword)) {
-            System.out.println("Пароль верный");
+        if (a == 0) {
+            System.out.println("Это не квадратное уравнение (a = 0)");
+            return;
+        }
+
+        double D = b * b - 4 * a * c;
+
+        if (D > 0) {
+            double x1 = (-b + Math.sqrt(D)) / (2 * a);
+            double x2 = (-b - Math.sqrt(D)) / (2 * a);
+
+            System.out.println("Два корня:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+
+        } else if (D == 0) {
+            double x = -b / (2 * a);
+            System.out.println("Один корень:");
+            System.out.println("x = " + x);
+
         } else {
-            System.out.println("Пароль неверный");
+            System.out.println("Корней нет (D < 0)");
         }
     }
 }
-
