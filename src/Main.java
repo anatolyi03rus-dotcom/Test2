dc907c5f56f0fece353f9bab9bb669844ed8af3e
Anatoly <anatolyi03rus@gmail.com>
2026-05-22 21:17:57 +0800

    zadanie5

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,32 @@
-//Testest
-
-//Создать объект класса Щенок, используя классы Животное, Собака.
-//Методы: вывести на консоль имя, подать голос, прыгать, бегать, кусать.
-import java.util.Scanner;
-class Animal {
-
-    String name;
-
-    public Animal(String name) {
-        this.name = name;
-    }
-
-    public void showName() {
-        System.out.println("Имя: " + name);
-    }
-}
-
-class Dog extends Animal {
-
-
-    public Dog(String name) {
-        super(name);
-    }
-
-    public void voice() {
-        System.out.println(name + " говорит: Гав-гав!");
-    }
-
-    public void run() {
-        System.out.println(name + " бегает");
-    }
-
-    public void bite() {
-        System.out.println(name + " кусается");
-    }
-}
-
-class Puppy extends Dog {
-
-
-    public Puppy(String name) {
-        super(name);
-    }
-
-    public void jump() {
-        System.out.println(name + " прыгает");
-    }
-}
-
 public class Main {
+
+    static class Phone {
+        String model;
+        int memory;
+
+        Phone(String model, int memory) {
+            this.model = model;
+            this.memory = memory;
+        }
+
+        void showInfo() {
+            System.out.println(model + " " + memory + "GB");
+        }
+    }
 
     public static void main(String[] args) {
 
-        Puppy puppy = new Puppy("Анатолий Б763");
+        Main.Phone phone1 =
+                new Phone("Samsung", 128);
 
-        puppy.showName();
-        puppy.voice();
-        puppy.jump();
-        puppy.run();
-        puppy.bite();
+        Main.Phone phone2 =
+                new Phone("iPhone", 256);
+
+        Main.Phone phone3 =
+                new Phone("Xiaomi", 64);
+
+        phone1.showInfo();
+        phone2.showInfo();
+        phone3.showInfo();
     }
 }
