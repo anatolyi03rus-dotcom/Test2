79dc7076c5188a95f762b6772ed89de5de9eff6e
Anatoly <anatolyi03rus@gmail.com>
2026-05-23 21:04:37 +0800

    zadanie6

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,143 @@
-public class Main {
+interface Building {
 
-    static class Phone {
-        String model;
-        int memory;
+    void build();                  // построить
+    double pricePerSquare();       // цена за метр квадра
+    int roomCount();               // количество комнат
+    void increaseArea(double area);// увеличить площадь
+    void rent();                   // сдать в аренду
+    void repair(String room);      // ремонт комнаты
+}
 
-        Phone(String model, int memory) {
-            this.model = model;
-            this.memory = memory;
-        }
 
-        void showInfo() {
-            System.out.println(model + " " + memory + "GB");
-        }
+class House implements Building {
+
+    double area;
+    int rooms;
+
+    House(double area, int rooms) {
+        this.area = area;
+        this.rooms = rooms;
     }
+
+    public void build() {
+        System.out.println("Дом построен");
+    }
+
+    public double pricePerSquare() {
+        return 1200;
+    }
+
+    public int roomCount() {
+        return rooms;
+    }
+
+    public void increaseArea(double area) {
+        this.area += area;
+        System.out.println("Площадь увеличена");
+    }
+
+    public void rent() {
+        System.out.println("Дом сдан в аренду");
+    }
+
+    public void repair(String room) {
+        System.out.println("Ремонт в комнате: " + room);
+    }
+
+
+    void garden() {
+        System.out.println("Во дворе есть сад");
+    }
+}
+
+
+class OfficeBuilding implements Building {
+
+    public void build() {
+        System.out.println("Офис построен");
+    }
+
+    public double pricePerSquare() {
+        return 2000;
+    }
+
+    public int roomCount() {
+        return 50;
+    }
+
+    public void increaseArea(double area) {
+        System.out.println("Офис расширен на " + area);
+    }
+
+    public void rent() {
+        System.out.println("Офис сдан");
+    }
+
+    public void repair(String room) {
+        System.out.println("Ремонт офиса: " + room);
+    }
+
+
+    void conferenceRoom() {
+        System.out.println("Есть конференц-зал");
+    }
+}
+
+
+
+class ShoppingCenter implements Building {
+
+    public void build() {
+        System.out.println("Торговый центр построен");
+    }
+
+    public double pricePerSquare() {
+        return 3000;
+    }
+
+    public int roomCount() {
+        return 100;
+    }
+
+    public void increaseArea(double area) {
+        System.out.println("ТЦ увеличен на " + area);
+    }
+
+    public void rent() {
+        System.out.println("ТЦ сдан в аренду");
+    }
+
+    public void repair(String room) {
+        System.out.println("Ремонт магазина: " + room);
+    }
+
+    void parking() {
+        System.out.println("Есть парковка");
+    }
+}
+
+public class Main {
 
     public static void main(String[] args) {
 
-        Main.Phone phone1 =
-                new Phone("Samsung", 128);
+        House house = new House(120, 5);
 
-        Main.Phone phone2 =
-                new Phone("iPhone", 256);
+        house.build();
+        System.out.println("Цена за м квадтратный: " + house.pricePerSquare());
+        System.out.println("Комнат: " + house.roomCount());
 
-        Main.Phone phone3 =
-                new Phone("Xiaomi", 64);
+        house.increaseArea(20);
+        house.rent();
+        house.repair("Кухня");
 
-        phone1.showInfo();
-        phone2.showInfo();
-        phone3.showInfo();
+        house.garden();
+
+        System.out.println();
+
+        OfficeBuilding office = new OfficeBuilding();
+        office.conferenceRoom();
+
+        ShoppingCenter mall = new ShoppingCenter();
+        mall.parking();
     }
 }
